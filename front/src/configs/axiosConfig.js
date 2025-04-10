4a164bc5d7ca40932985dbef22c48262ab288153
doosan5464 <doosan5464@gmail.com>
2025-04-10 17:49:11 +0900

    ?

--- front/src/configs/axiosConfig.js
+++ front/src/configs/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:8080",
-    baseURL: "http://kioskstudents.store",
+    baseURL: "https://kioskstudents.store",
 });
 
 api.interceptors.request.use(config => {
